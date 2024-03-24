7d67ee80edecc46fbcb6738523870a7c29a1688f
Estime Vangu <estimevangu.pro@gmail.com>
2024-03-24 21:59:45 +0100

    notif followers added still need to create it for the comments and likes

--- components/_body/_option/setting/Setting.jsx
+++ components/_body/_option/setting/Setting.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import { UserProfile, useUser, SignOutButton } from '@clerk/nextjs'
-import Image from 'next/image'
+import { UserProfile, useUser, SignOutButton, useClerk } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
 
 const Setting = () => {
 	const { isSignedIn, user } = useUser()
+	const { signOut } = useClerk()
+	const router = useRouter()
 
 	if (!isSignedIn) {
 		return null
@@ -20,9 +22,15 @@ const Setting = () => {
 					<div className="lg:border-t border-gray-400 py-5">
 						<UserProfile />
 						<div className="mt-4">
-							<SignOutButton>
-								<button className="btn-follow">log out</button>
-							</SignOutButton>
+							<button
+								className="btn-follow"
+								onClick={() => {
+									signOut()
+									router.push('/')
+								}}
+							>
+								Sign out
+							</button>
 							<span className="flex lg:mt-3 text-xs text-gray-400 uppercase mt-4">
 								*make sure to log out to save your changes
 							</span>
